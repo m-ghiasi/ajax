c61147b23644e6365f61b20791d93a94e73ee91f
Mahdieh Ghiasi <m.ghiasi@Son.ir>
2019-07-01 16:40:41 +0430

    t icon

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-var skip='0';
+    var skip = 0;
     $('.dashboard').addClass("select");
     $(".dashboard a span").addClass("white");
     $('.list-item li').on('click', function() {
@@ -13,16 +13,26 @@ var skip='0';
     $(".local_offer").on('click', function() {
 
         $(".co-table").addClass("display")
+        getTicket();
 
+
+
+        $('.next-page').on('click', function() {
+            skip += 10
+            getTicket();
+        })
+
+    });
+    function getTicket() {
         $.ajax({
             url: 'https://dev.map.ir/tbt/tickets',
             type: 'get',
             headers: {
                 "token": localStorage["token"]
             },
-            data:{
-                $top:'10',
-                $skip:skip
+            data: {
+                $top: 10,
+                $skip: skip
             }
 
 
@@ -46,20 +56,20 @@ var skip='0';
 
                 trs +=
                     `<tr>
-				<td>${index + 1}</td>
-				<td>${item.description}</td>
-				<td class="geom" data-lat="${item.geom.coordinates[0]}" data-lon="${item.geom.coordinates[1]}"></td>
-				<td class="status ${statusicon}"><div class="change-icon"><div class="icon"><div class="iconopen"></div><div <div class="iconopen">></div><div <div class="iconopen">></div></div></div></td>
-				</tr>`
+                <td>${skip + index + 1}</td>
+                <td>${item.description}</td>
+                <td class="geom" data-lat="${item.geom.coordinates[0]}" data-lon="${item.geom.coordinates[1]}"></td>
+                <td class="status ${statusicon}"><div class="change-icon"><div class="icon"><div class="iconopen"></div><div <div class="iconopen">></div><div <div class="iconopen">></div></div></div></td>
+                </tr>`
             });
             // window.location.assign('index.html');
 
             $('table tbody').html(trs);
 
-            $('.geom').on('click', function(){
-            	window.open(`https://map.ir/lat/${$(this).attr('data-lat')}/lng/${$(this).attr('data-lon')}/z/17`);
-});
+            $('.geom').on('click', function() {
+                window.open(`https://map.ir/lat/${$(this).attr('data-lat')}/lng/${$(this).attr('data-lon')}/z/17`);
+            });
         });
-    });
+    }
 
 });
